8e350283436e296943bf0c2b5236a8fe3124cfa1
Chichi-coder <lucie.macaigne@gmail.com>
2021-01-01 14:30:02 -0500

    added sliding sidebar, main design for dashboard

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,8 @@ const chart1 = document.getElementById('chart-1').getContext('2d');
 const chart2 = document.getElementById('chart-2').getContext('2d');
 const chart3 = document.getElementById('chart-3').getContext('2d');
 const dashTableBody = document.getElementById('dash-table-body');
+const sideBar = document.getElementsByClassName('sidebar')[0];
+const expandSidebar = document.getElementById('expand-sidebar');
 
 const array = [
     { item: 'Apple', quantity: 2, price: '$0.90', message: 'Once upon a time a cat knocked at my door and let hinself in' },
@@ -29,48 +31,86 @@ const apiKey = '5feddcaaa78ae44b49a6dca1';
 var barChart = new Chart(chart1, {
     type: 'bar',
     data: {
-        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges', 'Mango', 'Papaya'],
+        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges'],
         datasets: [{
             label: 'All the fruits',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [2, 4, 6, 1, 8, 10, 5, 2]
+            backgroundColor: ['rgb(192, 235, 252)', 'rgb(66, 135, 245)', 'rgb(55, 196, 250)', 'rgb(175, 247, 136)', 'rgb(90, 176, 209)', 'rgb(47, 247, 100)'],
+           
+            data: [2, 4, 6, 1, 8, 10]
         }]
     },
 
     options: {
         responsive: true,
         maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                },
+                gridLines: {
+                    color: 'white'
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    color: 'white'
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
     }
 });
 
 var lineChart = new Chart(chart2, {
     type: 'line',
     data: {
-        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges', 'Mango', 'Papaya'],
+        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges'],
         datasets: [{
-            label: 'All the fruits',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [2, 4, 6, 1, 8, 10, 6, 3]
+            
+            borderColor: 'blue',
+            pointBackgroundColor: 'blue',
+            backgroundColor: 'rgb(192, 235, 252)',
+            
+            data: [2, 4, 6, 1, 8, 10]
         }]
     },
 
     options: {
         responsive: true,
         maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                },
+                gridLines: {
+                    color: 'white'
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    color: 'white'
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
     }
 });
 
 var doughnutChart = new Chart(chart3, {
     type: 'doughnut',
     data: {
-        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges', 'Mango', 'Papaya'],
+        labels: ['Apples', 'Cherries', 'Peaches', 'Pears', 'Strawberries', 'Oranges'],
         datasets: [{
-            label: 'All the fruits',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [2, 4, 6, 1, 8, 10, 6, 3]
+            
+            backgroundColor: ['rgb(192, 235, 252)', 'rgb(66, 135, 245)', 'rgb(55, 196, 250)', 'rgb(175, 247, 136)', 'rgb(90, 176, 209)', 'rgb(47, 247, 100)'],
+            borderColor: ['rgb(192, 235, 252)', 'rgb(66, 135, 245)', 'rgb(55, 196, 250)', 'rgb(175, 247, 136)', 'rgb(90, 176, 209)', 'rgb(47, 247, 100)'],
+            data: [2, 4, 6, 1, 8, 10]
         }]
     },
 
@@ -95,4 +135,12 @@ function populateTable(wrapper, data) {
         
         wrapper.innerHTML += row;
     }
+}
+
+//['rgb(192, 235, 252)', 'rgb(66, 135, 245)', 'rgb(55, 196, 250)', 'rgb(175, 247, 136)', 'rgb(90, 176, 209)', 'rgb(47, 247, 100)']
+
+expandSidebar.addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
+    sideBar.classList.toggle('toggle-sidebar');
 }
